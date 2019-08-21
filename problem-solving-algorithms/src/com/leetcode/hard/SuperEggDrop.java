af1fe0bb73e87edc93964d7520a4ca306dd71488
Anand Raghunathan <anand.raghunathan@hotmail.com>
2019-08-21 12:24:38 +0530

    New problems solved

--- problem-solving-algorithms/src/com/leetcode/hard/SuperEggDrop.java
+++ problem-solving-algorithms/src/com/leetcode/hard/SuperEggDrop.java
@@ -14,6 +14,119 @@ package com.leetcode.hard;
  *         in our DP array (cache)
  */
 public class SuperEggDrop {
+
+    /**
+     * The below solution has turned the problem around from
+     * "How many moves do you need to check N floors if you have K eggs"
+     * to:
+     * "How many floors can you check given M moves available and K eggs".
+     *
+     * If you can solve this second problem than you can just increase the moves M one by one until you are able to
+     * check a number of floors larger or equal to the number N which the problem requires.
+     *
+     * He then defined
+     * dp[M][K] as the maximum number of floors that you can check within M moves given K eggs
+     *
+     * A move essentially is dropping an egg and it either breaks or doesn't break.
+     *
+     * Case A: The egg breaks and now you have spent 1 move (M=M-1) and also lost 1 egg (K=K-1). You can still check
+     * dp[M-1][K-1] floors, with your remaining eggs and moves.
+     *
+     * Case B: The egg remains and you only loose one move (M=M-1). You can still check dp[M-1][K] floors.
+     *
+     * Additionally you just checked a floor by dropping the egg from it.
+     *
+     * Therefore dp[M][K] = dp[M - 1][k - 1] + dp[M - 1][K] + 1
+     *
+     * As you can see we can easily calculate how many floors we can check in M moves if we know how many floors we can check in M-1 moves.
+     *
+     * However we not only have to know how many floors we can can check with one move less, but also how many we can check with one move and one egg less. Therefore we have to calculate how many moves we can check for all number off eggs from 1 to K.
+     *
+     * An example:
+     * N = 6 and K = 2
+     * Turn the problem around: How many floors can you check with 2 eggs and M moves:
+     *
+     * Solve for M=1, K=1,2
+     * you can only check 1 floor (since afterwards you have no more moves left)
+     *
+     * Solve for M=2, K=1
+     * Case A: Your egg breaks, you have no more eggs left and can check nothing. dp[M=1,K=0]=0
+     * Case B: your egg survives and you can use it to test an additional floor above the floor you just tested. dp[M=1,K=1]=1
+     * dp[2][1]=dp[1][0]+dp[1][1]+1=0+1+1=2
+     *
+     * Solve for M=2, K=2
+     * Case A: Your egg breaks: you have 1 move left and 1 egg. Since you know that the floor F where the eggs break is
+     * below the floor you just tested you can now check dp[M=1,K=1] floors below you, with only 1 move left you check
+     * 1 additional floor below. dp[M=1,K=1]=1
+     * Case B: Your eggs survives and you start to search above the current floor. dp[1][2] is still only 1 move and
+     * we can check 1 floor. dp[1][2]
+     *
+     * dp[2][2]=1+1+1=3
+     *
+     * Solve for M=3, K=1
+     * Case A: Your egg breaks and you are out of eggs, no chance to check anything anymore
+     * Case B: Your egg survives and you can use it for 2 more moves dp[2][1], which as we established above is enough
+     * to check 2 floors.
+     *
+     * dp[3][1]=0+2+1=3
+     *
+     * Solve for M=3, K=2
+     * Case A: Your egg breaks and you check dp[2][1]=2 additional floors
+     * Case B: Your egg survives and you check dp[2][2]=3 additional floors
+     * dp[3][2]=2+3+1=6
+     *
+     * As you can see 3 moves and 2 eggs allows you to check 6 floors. Which answers the original question how many
+     * moves you need to check 6 floors given 2 eggs,
+     * The answer is 3
+     *
+     * I hope this helps to make it more clear.
+     *
+     */
+    public int superEggDropBestOptimized(int K, int N) {
+        int[][] dp = new int[N + 1][K + 1];
+        int m = 0;
+        while (dp[m][K] < N) {
+            m++;
+            for (int k = 1; k <= K; ++k)
+                dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1;
+        }
+        return m;
+    }
+
+    /**
+     *
+     * Solutions tab - Approach 2
+     */
+    public int superEggDropOptimized(int K, int N) {
+        // Right now, dp[i] represents dp(1, i)
+        int[] dp = new int[N+1];
+
+        // Egg == 1, Floor == N. We still make N moves in the worst case
+        for (int i = 1; i <= N; i++)
+            dp[i] = i;
+
+        for (int k = 2; k <= K; ++k) {
+            // Now, we will develop dp2[i] = dp(k, i)
+            int[] dp2 = new int[N+1];
+            int x = 1;
+            for (int n = 1; n <= N; ++n) {
+                // Let's find dp2[n] = dp(k, n)
+                // Increase our optimal x while we can make our answer better.
+                // Notice max(dp[x-1], dp2[n-x]) > max(dp[x], dp2[n-x-1])
+                // is simply max(T1(x-1), T2(x-1)) > max(T1(x), T2(x)).
+                while (x < n && Math.max(dp[x-1], dp2[n-x]) > Math.max(dp[x], dp2[n-x-1]))
+                    x++;
+
+                // The final answer happens at this x.
+                dp2[n] = 1 + Math.max(dp[x-1], dp2[n-x]);
+            }
+
+            dp = dp2;
+        }
+
+        return dp[N];
+    }
+
     public int superEggDrop(int totalEggs, int totalFloors) {
         /*
           We do +1 to index off of 1. So that the final answer that
@@ -103,6 +216,8 @@ public class SuperEggDrop {
 
     public static void main(String[] args) {
         SuperEggDrop obj = new SuperEggDrop();
-        System.out.println(obj.superEggDrop(2, 6));
+        //System.out.println(obj.superEggDrop(2, 6));
+        //System.out.println(obj.superEggDropOptimized(2, 6));
+        System.out.println(obj.superEggDropBestOptimized(2, 6));
     }
 }
