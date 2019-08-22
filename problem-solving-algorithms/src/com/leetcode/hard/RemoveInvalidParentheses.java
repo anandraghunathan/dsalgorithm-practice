f2d053abfd6c5f17c3182b1fdc6b0e1be37af80f
Anand Raghunathan <anand.raghunathan@hotmail.com>
2019-08-22 17:32:18 +0530

    Modified file

--- problem-solving-algorithms/src/com/leetcode/hard/RemoveInvalidParentheses.java
+++ problem-solving-algorithms/src/com/leetcode/hard/RemoveInvalidParentheses.java
@@ -3,36 +3,48 @@ package com.leetcode.hard;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * https://leetcode.com/problems/remove-invalid-parentheses/
+ *
+ * Time and space yet to research
+ */
 public class RemoveInvalidParentheses {
     public List<String> removeInvalidParentheses(String s) {
-        List<String> output = new ArrayList<>();
-        removeHelper(s, output, 0, 0, '(', ')');
-        return output;
+        List<String> res = new ArrayList<>();
+        removeHelper(s, res, 0, 0, '(', ')');
+        return res;
     }
 
-    public void removeHelper(String s, List<String> output, int iStart, int jStart, char openParen, char closedParen) {
-        int numOpenParen = 0, numClosedParen = 0;
+    public void removeHelper(String s, List<String> res, int iStart, int jStart, char openParen, char closedParen) {
+        int openParenCount = 0, closedParenCount = 0;
+        // Count the number of openParen and closedParen in the given string to compute if the closedParen is > openParen
         for (int i = iStart; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == openParen)
-                numOpenParen++;
+                openParenCount++;
             if (c == closedParen)
-                numClosedParen++;
+                closedParenCount++;
 
-            if (numClosedParen > numOpenParen) { // We have an extra closed paren we need to remove
+            // If the closedParenCount > than openParenCount, then we inspect the string from the jStart's position
+            if (closedParenCount > openParenCount) { // We have an extra closed paren we need to remove
                 for (int j = jStart; j <= i; j++) // Try removing one at each position, skipping duplicates
+                    /*
+                        If the current char at j is a closedParen
+                        and j is the start index or the previous character is not a closedParen
+                     */
                     if (s.charAt(j) == closedParen && (j == jStart || s.charAt(j - 1) != closedParen))
                         // Recursion: iStart = i since we now have valid # closed parenthesis thru i. jStart = j prevents duplicates
-                        removeHelper(s.substring(0, j) + s.substring(j + 1, s.length()), output, i, j, openParen, closedParen);
+                        removeHelper(s.substring(0, j) + s.substring(j + 1, s.length()), res, i, j, openParen, closedParen);
                 return; // Stop here. The recursive calls handle the rest of the string.
             }
         }
         // No invalid closed parenthesis detected. Now check opposite direction, or reverse back to original direction.
         String reversed = new StringBuilder(s).reverse().toString();
         if (openParen == '(')
-            removeHelper(reversed, output, 0, 0, ')','(');
+            // Reset the iStart and jStart to 0 cause we have to inspect the string as a fresh start
+            removeHelper(reversed, res, 0, 0, ')','(');
         else
-            output.add(reversed);
+            res.add(reversed);
     }
 
     public static void main(String[] args) {
