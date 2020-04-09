166bf39a296d0ea4b5960fc56c2f6721b87b23a3
Anand Raghunathan <anand.raghunathan@hotmail.com>
2020-04-08 17:40:59 -0700

    New problems solved.

---
+++ problem-solving-algorithms/src/com/leetcode/array/FindPivotIndex.java
@@ -0,0 +1,26 @@
+package com.leetcode.array;
+
+/**
+ * https://leetcode.com/problems/find-pivot-index/
+ */
+public class FindPivotIndex {
+    public static int pivotIndex(int[] nums) {
+        int leftSum = 0, sum = 0;
+        for(int num : nums) {
+            sum += num;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(leftSum == sum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pivotIndex(new int[]{1, 7, 3, 6, 5, 6}));
+        System.out.println(pivotIndex(new int[]{1, 2, 3}));
+    }
+}
